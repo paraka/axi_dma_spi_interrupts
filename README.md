100bec25286fcc4549490fc1a36e365039c53b9a
Sergio Paracuellos <sergio.paracuellos@gmail.com>
2017-12-08 15:10:44 +0100

    axi-dma: vivado and SDK project files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# axi_dma_spi_interrupts
-AXI-DMA IP with SPI interrupts using concat and a FIFO to get a loopback
+# AXI DMA with SPI interrupts
+
+This repository included an example of how to use AXI DMA IP core design and xilinx bare-metal library.
+
+It is using DMA in scatter gather mode using interrupts. To create a loopback AXI FIFO is being used also in FPGA design.
+
+# Design
+
+I am using Shared peripheral interrups (SPI) wth a concat in this design. 
+
+![](images/axi-dma-irqs.png?raw=true)
+
+# Versions
+
+Vivado version for this design is 2014.4.
+
+# Target platform
+
+Target platform is zynq 7000 zedboard.
+
+# Compile and run sample
+
+Just use Xilinx SDK with the project is included in this repo and there should not be any problem to make a correct deploy.
+
+Enjoy!
+
+Thanks to fpgadeveloper and xilinx samples for inspiration.
---
+++ dma_fifo_loopback.sdk/dma_fifo_loopback/Debug/src/dma_test.d
@@ -0,0 +1,64 @@
+src/dma_test.o src/dma_test.d: ../src/dma_test.c \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_exception.h
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../dma_fifo_loopback_bsp/ps7_cortexa9_0/include/xil_exception.h:
